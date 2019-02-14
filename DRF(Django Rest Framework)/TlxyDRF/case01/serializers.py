54ddeb4d0df63bbd4a19534218dfb123fc2c6bed
xuxaut555 <xuxaut668@163.com>
2019-02-15 01:11:10 +0800

    Add Django Rest Framework

---
+++ DRF(Django Rest Framework)/TlxyDRF/case01/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from case01.models import *
+
+class StudentSer(serializers.ModelSerializer):
+    class Meta:
+        #告诉序列化器，对应哪个模型
+        model =  Student
+        fields = '__all__'
